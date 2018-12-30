e9a92fae692d073833ff37191ddcd1bb9888a124
ashish pal <ashish456747.gmail.com>
2018-12-30 19:40:34 +0530

    Lacal and Global Variables

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,14 @@
 public class HelloWorld{
+	static int test(int x){
+		x=5;
+		System.out.println("from test: " x);
+	}
 	public static void main (String args[]){
+		System.out.println("Main Start");
 		System.out.println("Hello World");
+		int x=0;
+		test(x);
+		System.out.println("from test: " x);
 	}
 
 }
