6849b4e6c68ac7a9e453183441be0606068932b0
ashish pal <ashish456747.gmail.com>
2019-09-22 21:05:23 +0530

    modified message

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,10 +5,11 @@ public class HelloWorld{
 	}
 	public static void main (String args[]){
 		System.out.println("Main Start");
-		System.out.println("Hello World!!!!");
+		System.out.println("Hello World");
 		int x=0;
 		test(x);
 		System.out.println("from test: "+x);
+		System.out.println("Bye!!!!");
 	}
 
 }
